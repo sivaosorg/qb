18dac28060e94b6decedd3625ed93bcb22bc2778
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2023-10-01 11:45:30 +0700

    :wrench: chore: added join types based on condition args #9

--- qb_join.go
+++ qb_join.go
@@ -2,15 +2,17 @@ package qb
 
 import "fmt"
 
-func (q *QbDB) buildJoin(joinType, table, on string) *QbDB {
-	// q.Builder.join = append(q.Builder.join, " "+joinType+" JOIN "+table+" ON "+on+" ")
-	q.Builder.join = append(q.Builder.join, fmt.Sprintf("%s%s%s%s%s%s%s", " ", joinType, " JOIN ", table, " ON ", on, " "))
-	return q
-}
-
 // InnerJoin joins tables by getting elements if found in both
 func (q *QbDB) InnerJoin(table, left, operator, right string) *QbDB {
 	return q.buildJoin(JoinInner, table, left+operator+right)
+}
+
+// InnerJoinIf joins tables by getting elements if found in both
+func (q *QbDB) InnerJoinIf(fnc func() bool, table, left, operator, right string) *QbDB {
+	if !fnc() {
+		return q
+	}
+	return q.InnerJoin(table, left, operator, right)
 }
 
 // LeftJoin joins tables by getting elements from left without those that null on the right
@@ -18,9 +20,25 @@ func (q *QbDB) LeftJoin(table, left, operator, right string) *QbDB {
 	return q.buildJoin(JoinLeft, table, left+operator+right)
 }
 
+// LeftJoinIf joins tables by getting elements from left without those that null on the right
+func (q *QbDB) LeftJoinIf(fnc func() bool, table, left, operator, right string) *QbDB {
+	if !fnc() {
+		return q
+	}
+	return q.LeftJoin(table, left, operator, right)
+}
+
 // RightJoin joins tables by getting elements from right without those that null on the left
 func (q *QbDB) RightJoin(table, left, operator, right string) *QbDB {
 	return q.buildJoin(JoinRight, table, left+operator+right)
+}
+
+// RightJoinIf joins tables by getting elements from right without those that null on the left
+func (q *QbDB) RightJoinIf(fnc func() bool, table, left, operator, right string) *QbDB {
+	if !fnc() {
+		return q
+	}
+	return q.RightJoin(table, left, operator, right)
 }
 
 // FullJoin joins tables by getting all elements of both sets
@@ -28,9 +46,30 @@ func (q *QbDB) FullJoin(table, left, operator, right string) *QbDB {
 	return q.buildJoin(JoinFull, table, left+operator+right)
 }
 
+// FullJoinIf joins tables by getting all elements of both sets
+func (q *QbDB) FullJoinIf(fnc func() bool, table, left, operator, right string) *QbDB {
+	if !fnc() {
+		return q
+	}
+	return q.FullJoin(table, left, operator, right)
+}
+
 // FullOuterJoin joins tables by getting an outer sets
 func (q *QbDB) FullOuterJoin(table, left, operator, right string) *QbDB {
 	return q.buildJoin(JoinFullOuter, table, left+operator+right)
+}
+
+// FullOuterJoinIf joins tables by getting an outer sets
+func (q *QbDB) FullOuterJoinIf(fnc func() bool, table, left, operator, right string) *QbDB {
+	if !fnc() {
+		return q
+	}
+	return q.FullOuterJoin(table, left, operator, right)
+}
+
+func (q *QbDB) buildJoin(joinType, table, on string) *QbDB {
+	q.Builder.join = append(q.Builder.join, fmt.Sprintf("%s%s%s%s%s%s%s", " ", joinType, " JOIN ", table, " ON ", on, " "))
+	return q
 }
 
 // Todo
